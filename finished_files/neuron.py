e4b783690e8c2c2f32ebfb54758e4635d26217a5
kora11e <87006301+kora11e@users.noreply.github.com>
2025-07-16 19:06:53 +0200

    update

--- finished_files/neuron.py
+++ finished_files/neuron.py
@@ -1,7 +1,22 @@
-import math
 import torch
 import torch.nn as nn
+from torch import Tensor
+import numpy as np
+
+__all__ = [
+    'HCRNN_Neuron'
+]
 
 class Neuron(nn.Module):
+
+    '''
+    definicja stałych określająca rozmiar wejścia i wyjścia dla liczb neuronów.
+    '''
+
+    __constants__ = ['in_features', 'out_features']
+    __methods__ = ['gaussian']
+    in_features: int
+    out_features: int
+
     def __init__(self):
         pass
