6f7e94398f933948bed97207475098ccbb9b43a2
kora11e <87006301+kora11e@users.noreply.github.com>
2025-07-21 15:26:23 +0200

    update untitest podzielony

---
+++ finished_files/unit_tests.py
@@ -0,0 +1,56 @@
+import unittest
+
+class CDFTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+
+class CDFTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+class JointDistributionTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+
+class EstimationTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+
+class ConditionalEstimationTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+
+class PropagationTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+
+class CDFTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+class EntropyTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+class MITest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+class EMATest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+class OptimizationTest(unittest.TestCase):
+    def test_starting_out(self):
+        pass
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
