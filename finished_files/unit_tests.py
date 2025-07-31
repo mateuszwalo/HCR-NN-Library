0bb17e8537f82f5843ddb55ca595bed11950e6ae
kora11e <87006301+kora11e@users.noreply.github.com>
2025-07-31 18:52:40 +0200

    update testing

--- finished_files/unit_tests.py
+++ finished_files/unit_tests.py
@@ -1,13 +1,10 @@
 import unittest
 
-class CDFTest(unittest.TestCase):
-    def test_starting_out(self):
-        pass
-
 
 class CDFTest(unittest.TestCase):
     def test_starting_out(self):
         pass
+
 
 class JointDistributionTest(unittest.TestCase):
     def test_starting_out(self):
@@ -29,28 +26,30 @@ class PropagationTest(unittest.TestCase):
         pass
 
 
-class CDFTest(unittest.TestCase):
-    def test_starting_out(self):
-        pass
-
 class EntropyTest(unittest.TestCase):
     def test_starting_out(self):
         pass
+
 
 class MITest(unittest.TestCase):
     def test_starting_out(self):
         pass
 
+
 class EMATest(unittest.TestCase):
     def test_starting_out(self):
         pass
 
+
 class OptimizationTest(unittest.TestCase):
     def test_starting_out(self):
         pass
+    
+
 
 def main():
     unittest.main()
+
 
 if __name__ == "__main__":
     main()
