484cf732886df1553a06ab01e75117c4b4762d66
kora11e <87006301+kora11e@users.noreply.github.com>
2025-08-09 21:34:16 +0200

    update stock funciton
    Szybki quick fix

--- data/data_fetch.py
+++ data/data_fetch.py
@@ -1,16 +1,18 @@
 import yfinance as yf
+import pandas as pd
 
 """
 To jest skrypt do wyciągania danych giełdowych. 
 Aby go odaplić trzeba zmienić kod spółki giełdowej i przedziały czasowe.
 """
 
-kod:str = "OTGLF"
-start:str = "2015-01-01"
-end:str = "2025-01-01"
+def fetch_stock_data(kod:str = "OTGLF",
+                     start:str = "2015-01-01",
+                     end:str = "2025-01-01",
+                     save_path:str = './') -> None:
 
-data = yf.download(kod, start=start, end=end)
+    data = yf.download(kod, start=start, end=end)
 
-data.to_csv("CDPR_stock_data.csv")
+    data.to_csv(f"{save_path}{kod}_stock_data.csv")
 
-print("Data saved to CDPR_stock_data.csv")
+    print(f"Data saved to {kod}_stock_data.csv")
