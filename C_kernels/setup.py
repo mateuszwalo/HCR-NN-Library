ae02773ccda88e2e549b75b65eb9911a94728280
kora11e <87006301+kora11e@users.noreply.github.com>
2025-09-04 21:08:35 +0200

    updare

--- C_kernels/setup.py
+++ C_kernels/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name="layers_kernels",
+    name="HCR_kernels",
     ext_modules=[
         CUDAExtension(
-            name="layers_kernels",
+            name="HCR_kernels",
             sources=["layers_kernel_bindings.cpp", 
                      "mean_estimation_kernel_wrapper.cu",
                      "conditional_estimation_kernel.cu",
@@ -14,6 +14,9 @@ setup(
                      "entropy_mi_kernel.cu",
                      "base_optimization.cu"
                      ],
+            extra_compile_args={'cxx': ['-g'],
+                    'nvcc': ['-O2']},
+            extra_link_args=['-Wl,--no-as-needed', "-s"]
         )
     ],
     cmdclass={"build_ext": BuildExtension}
