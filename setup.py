4a8a365490c597b664965ea86c21adf05f1b1e7a
kora11e <87006301+kora11e@users.noreply.github.com>
2025-10-09 19:18:37 +0200

    update tests included setup
    Dodałem setup.cfg do testowania paczki

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ with open('README.md', 'r+', encoding="utf8") as f:
 
 setup(
     name='hcr_nn',
-    version='1.0.1',
+    version='1.0.3',
+    description='Package implementing Hierarchical Correlation Reconstruction Methodologies',
     packages=find_packages(),
+    #setup_requires=['pytest-runner', 'flake8'],
+    #tests_require=['pytest'],
     install_requires=[
         'numpy>=1.26.4',
         'matplotlib>=3.10.1',
         'scipy>=1.13.1',
         'torch>=2.7.0',
+        'pandas>=2.3.0',
     ],
     entry_points={
         "console_scripts": [
