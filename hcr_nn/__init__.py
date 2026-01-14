a7e4504d41e54215148d3f24b4dfeeef9bfdbcab
kora11e <87006301+kora11e@users.noreply.github.com>
2026-01-14 19:08:05 +0100

    update toml file

--- hcr_nn/__init__.py
+++ hcr_nn/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ['layers', 'layers_optimized', 'basis', 'models', 'neuron', 'density', 'layers_kernel']
+__all__ = ['layers', 'basis', 'models', 'neuron', 'density']
 from .layers import *
-from .layers_optimized import *
+#from .layers_optimized import *
 from .basis import *
 from .models import *
 from .neuron import *
 from .density import *
-from .layers_kernel import *
+#from .layers_kernel import *
